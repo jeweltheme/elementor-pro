20c97ead65f5b7d40f691ca10917db458529e717
Christos Chiotis <chiotis@gmail.com>
2018-12-04 19:48:31 +0200

    update to 2.2.4

--- modules/global-widget/documents/widget.php
+++ modules/global-widget/documents/widget.php
@@ -2,6 +2,7 @@
 namespace ElementorPro\Modules\GlobalWidget\Documents;
 
 use Elementor\Modules\Library\Documents\Library_Document;
+use Elementor\User;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
@@ -24,5 +25,9 @@ class Widget extends Library_Document {
 
 	public static function get_title() {
 		return __( 'Global Widget', 'elementor-pro' );
+	}
+
+	public function is_editable_by_current_user() {
+		return User::is_current_user_can_edit( $this->get_main_id() );
 	}
 }
