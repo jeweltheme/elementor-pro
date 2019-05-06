27036175d69f3570484e7bbbcaa0e68444db202a
Christos Chiotis <chiotis@gmail.com>
2019-05-06 15:52:14 +0300

    update to 2.5.7

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -1,4 +1,4 @@
-/*! elementor-pro - v2.5.5 - 08-04-2019 */
+/*! elementor-pro - v2.5.7 - 05-05-2019 */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
@@ -983,7 +983,7 @@ var ElementorProFrontend = function (_elementorModules$Vie) {
 			};
 
 			jQuery.each(handlers, function (moduleName, ModuleClass) {
-				_this2.modules[moduleName] = new ModuleClass(jQuery);
+				_this2.modules[moduleName] = new ModuleClass();
 			});
 		}
 	}, {
@@ -1344,25 +1344,27 @@ var _class = function (_elementorModules$fro) {
 	}, {
 		key: 'setEntranceAnimation',
 		value: function setEntranceAnimation() {
-			var modal = this.getModal(),
-			    $widgetContent = modal.getElements('widgetContent'),
-			    deviceMode = elementorFrontend.getCurrentDeviceMode();
-
-			var settingKey = 'entrance_animation';
-
-			if ('desktop' !== deviceMode) {
-				settingKey = settingKey + '_' + deviceMode;
-			}
-
-			var newAnimation = this.getDocumentSettings(settingKey);
+			var $widgetContent = this.getModal().getElements('widgetContent'),
+			    settings = this.getDocumentSettings(),
+			    newAnimation = elementorFrontend.getCurrentDeviceSetting(settings, 'entrance_animation');
 
 			if (this.currentAnimation) {
 				$widgetContent.removeClass(this.currentAnimation);
 			}
 
+			if (!newAnimation) {
+				return;
+			}
+
+			var animationDuration = settings.entrance_animation_duration.size;
+
 			this.currentAnimation = newAnimation;
 
 			$widgetContent.addClass(newAnimation);
+
+			setTimeout(function () {
+				return $widgetContent.removeClass(newAnimation);
+			}, animationDuration * 1000);
 		}
 	}, {
 		key: 'initModal',
@@ -1405,6 +1407,8 @@ var _class = function (_elementorModules$fro) {
 							if (settings.timing && settings.timing.times_count) {
 								_this2.countTimes();
 							}
+
+							_this2.$element.remove();
 						}
 					});
 
